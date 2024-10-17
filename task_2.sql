fb4e1f38f9850f2caaa5a215ec234628e82da49f
Blessing Denis <fargoxyt@gmail.com>
2024-10-17 14:54:17 +0100

    made changes

--- task_2.sql
+++ task_2.sql
@@ -22,13 +22,13 @@ CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+    CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 CREATE TABLE Order_Details (
     orderdetail_id INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-    CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES Books (book_id)
+    CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
